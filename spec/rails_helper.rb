4ed57512f58a0c4ab15ae6d566931d963716f165
Mandy Real <normandy.real@gmail.com>
2017-04-29 14:11:01 +1000

    added spec for todo request and updated routes

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,11 @@ require 'spec_helper'
 require 'rspec/rails'
 require 'database_cleaner'
 
+
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+
+ActiveRecord::Migration.maintain_test_schema!
+
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
     with.test_framework :rspec
@@ -15,6 +20,9 @@ Shoulda::Matchers.configure do |config|
 end
 
 RSpec.configuration do |config|
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.use_transactional_fixtures = true
+
   # [...]
   # add `FactoryGirl` methods
   config.include FactoryGirl::Syntax::Methods
@@ -32,6 +40,14 @@ RSpec.configuration do |config|
     end
   end
   # [...]
+  config.infer_spec_type_from_file_location!
+
+  # arbitrary gems may also be filtered via:
+  # config.filter_gems_from_backtrace("gem name")
+  config.include RequestSpecHelper, type: :request
+  # Filter lines from Rails gems in backtraces.
+  config.filter_rails_from_backtrace!
+
 end
 
 # Add additional requires below this line. Rails is not loaded until this point!
@@ -49,38 +65,7 @@ end
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 # Checks for pending migration and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
-ActiveRecord::Migration.maintain_test_schema!
 
-RSpec.configure do |config|
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
-  # instead of true.
-  config.use_transactional_fixtures = true
-
-  # RSpec Rails can automatically mix in different behaviours to your tests
-  # based on their file location, for example enabling you to call `get` and
-  # `post` in specs under `spec/controllers`.
-  #
-  # You can disable this behaviour by removing the line below, and instead
-  # explicitly tag your specs with their type, e.g.:
-  #
-  #     RSpec.describe UsersController, :type => :controller do
-  #       # ...
-  #     end
-  #
-  # The different available types are documented in the features, such as in
-  # https://relishapp.com/rspec/rspec-rails/docs
-  config.infer_spec_type_from_file_location!
-
-  # Filter lines from Rails gems in backtraces.
-  config.filter_rails_from_backtrace!
-  # arbitrary gems may also be filtered via:
-  # config.filter_gems_from_backtrace("gem name")
-end
