53d8efd251b0e084326963a57c90518486f5a783
Mandy Real <normandy.real@gmail.com>
2017-04-30 11:38:28 +1000

    added spec and methods for todo items

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -1,14 +1,17 @@
 module ExceptionHandler
-  # provides the more graceful `included` method
   extend ActiveSupport::Concern
 
   included do
+    rescue_from ActiveRecord::RecordInvalid, with: :four_twenty_two
+
     rescue_from ActiveRecord::RecordNotFound do |e|
       json_response({ message: e.message }, :not_found)
     end
+  end
 
-    rescue_from ActiveRecord::RecordInvalid do |e|
-      json_response({ message: e.message }, :unprocessable_entity)
-    end
+  private
+
+  def four_twenty_two(e)
+    json_response({ message: e.message }, :unprocessable_entity)
   end
 end
